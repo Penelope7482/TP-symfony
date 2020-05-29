103a4ee84d3d92223d34d70dbefe80f4a7884daf
Pénélope PRINGALLE <penelopepringalle@gmail.com>
2020-05-29 15:28:45 +0200

    crud category, lien entre les tables

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -35,30 +35,17 @@ class ArticleController extends AbstractController
             'articles' => $articles
         ]);
     }
-    /**
-     * Afficher le formulaire de création d'un article
-     * 
-     * @Route("/create", name="article_create", methods={"GET"})
-     */
-    public function create()
-    {
-        return $this->render("/article/create.html.twig");
-    }
-
-    /**
-     * Affficher un article
-     * 
-     * @Route("/{article}", name="article_show", methods={"GET"})
-     */
-    public function show($article)
-    {
-        $articleRepository = $this->getDoctrine()->getRepository(Article::class);
-        $article = $articleRepository->find($article);
-
-        return $this->render("/article/show.html.twig", [
-            'article' => $article
-        ]);
-    }
+  //  /**
+  //   * Afficher le formulaire de création d'un article
+  //   * 
+  //   * @Route("/create", name="article_create", methods={"GET"})
+  //   */
+  //  public function create()
+  //  {
+  //      return $this->render("/article/create.html.twig");
+  //  }
+//
+ 
 
  //  /**
  //   * Traiter le formulaire de création d'un article
@@ -116,59 +103,116 @@ public function new(Request $request): Response
 
     // CAS GET ou CAS POST SI FORMULAIRE INVALIDE (if ci-dessus) :
     // On affiche le formulaire
-        return $this->render('product/new.html.twig', [
+        return $this->render('article/create.html.twig', [
             'product' => $article,
             'form' => $form->createView(),
         ]);
     }
 
-    /**
-     * @Route("/{article}/edit/", name="article_edit", methods={"GET"})
-     * 
-     */
-    public function edit($article)
-    {
-        $articleRepository = $this->getDoctrine()->getRepository(Article::class);
-        $article = $articleRepository->find($article);
+ //  /**
+ //   * @Route("/{article}/edit/", name="article_edit", methods={"GET"})
+ //   * 
+ //   */
+ //  public function edit($article)
+ //  {
+ //      $articleRepository = $this->getDoctrine()->getRepository(Article::class);
+ //      $article = $articleRepository->find($article);
 
-        return $this->render("/article/create.html.twig", [
-            'article' => $article
+ //      return $this->render("/article/create.html.twig", [
+ //          'article' => $article
+ //      ]);
+ //  }
+
+ //  /**
+ //   * @Route("/{article}/edit", name="article_update", methods={"POST"})
+ //   */
+ //  public function update(Request $request, Article $article)
+ //  {
+
+
+ //      $articleRepository = $this->getDoctrine()->getRepository(Article::class);
+ //      $article = $articleRepository->find($article);
+ //      $article->setTitle($_POST['title']);
+ //      $article->setContent($_POST['content']);
+ //      $article->setShortContent($_POST['short_content']);
+ //      // On récupère l'EntityManager du service Doctrine :
+ //      // Notez que le code est plus court que dans l'expliation ci-dessus !
+ //      $manager = $this->getDoctrine()->getManager();
+
+ //      // On donne l'object en gestion à Doctrine pour qu'il "persiste" l'object, c'est à dire qu'il prépare la requête
+ //      $manager->persist($article);
+
+ //      // On execute effectivement la requête :
+ //      $manager->flush();
+ //      return $this->redirectToRoute("article_index");
+ //  }
+
+
+/**
+* @Route("/{article}/update", name="product_update", methods={"GET","POST"})
+*/
+public function update(Request $request, Article $article): Response
+    {
+
+    // CAS GET (affichage) :
+        // On prépare l'article à créer avec le formulaire
+        
+
+        // On prépare le formulaire : on utilise le service createForm avec en arguments: le formulaire généré (ArticleType) et l'objet traité par le formulaire ($article)
+        $form = $this->createForm(ArticleType::class, $article);
+
+    // CAS POST (traitement) :
+        // On indique au formulaire de traiter la requête
+        $form->handleRequest($request);
+
+        // Si le formulaire a été envoyé et est valide, on le traite
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // On enregistre la donnée
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            // On redirige vers la page article_index
+            return $this->redirectToRoute('article_index');
+        }
+
+    // CAS GET ou CAS POST SI FORMULAIRE INVALIDE (if ci-dessus) :
+    // On affiche le formulaire
+        return $this->render('article/create.html.twig', [
+            'product' => $article,
+            'form' => $form->createView(),
         ]);
     }
 
-    /**
-     * @Route("/{article}/edit", name="article_update", methods={"POST"})
+
+
+   /**
+    * @Route("/{article}/delete", name="article_delete", methods={"POST"})
+    * 
+    */
+   public function delete(Request $request, Article $article)
+   {
+     $manager = $this->getDoctrine()->getManager();
+       $manager->remove($article);
+       $manager->flush();
+       return $this->redirectToRoute("article_index");
+   }
+
+ 
+
+       /**
+     * Afficher un article
+     * 
+     * @Route("/{article}", name="article_show", methods={"GET"})
      */
-    public function update(Request $request, Article $article)
+    public function show($article)
     {
-
-
         $articleRepository = $this->getDoctrine()->getRepository(Article::class);
         $article = $articleRepository->find($article);
-        $article->setTitle($_POST['title']);
-        $article->setContent($_POST['content']);
-        $article->setShortContent($_POST['short_content']);
-        // On récupère l'EntityManager du service Doctrine :
-        // Notez que le code est plus court que dans l'expliation ci-dessus !
-        $manager = $this->getDoctrine()->getManager();
 
-        // On donne l'object en gestion à Doctrine pour qu'il "persiste" l'object, c'est à dire qu'il prépare la requête
-        $manager->persist($article);
-
-        // On execute effectivement la requête :
-        $manager->flush();
-        return $this->redirectToRoute("article_index");
-    }
-
-    /**
-     * @Route("/{article}/delete", name="article_delete", methods={"POST"})
-     * 
-     */
-    public function delete(Request $request, Article $article)
-    {
-      $manager = $this->getDoctrine()->getManager();
-        $manager->remove($article);
-        $manager->flush();
-        return $this->redirectToRoute("article_index");
+        return $this->render("/article/show.html.twig", [
+            'article' => $article
+        ]);
     }
 }
